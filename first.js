84d325530e14dee3b192c12e92445b23604bc442
Muskangupta2909 <muskaangupta95881@gmail.com>
2024-10-03 11:52:47 +0530

    push

---
+++ first.js
@@ -0,0 +1,65 @@
+//object non primitive data type
+const student ={
+    fullName : "muskaan gupta",
+    age : 23,
+    cgpa : 7.87,
+    isPass : true
+};
+ console.log(student);
+
+//arithmetic operator + ,-,*,/
+let a=4;
+let b=5;
+console.log("a =",a, "b =",b);
+
+console.log("a + b =",a+b);
+console.log("a - b =",a-b);
+console.log("a * b =",a*b);
+console.log("a / b =",a/b);
+console.log("a % b =",a%b);
+console.log("a ** b =",a**b);
+console.log("a ^ b =",a^b);
+
+
+/*assignment operator
+= , += , -=*/
+
+a += 5;  //a = a + 5
+console.log("a =",a);
+
+
+
+//conditional statement 
+//if statement 
+
+ let mode ="dark";
+ let color;
+
+ if (mode === "dark") {
+    color = "black";
+ }
+ if (mode === "light"){
+    color ="white";
+ }
+ console.log(color);
+
+ //if_else statement 
+  
+ if (mode === "dark") {
+    color = "black";
+ }
+
+ else {
+    color = "black";
+ }
+ console.log(color);
+
+ let age = 14;
+ 
+ if (age >18){
+    console.log("vote");
+ }
+ else{
+    console.log("no");
+ }
+
